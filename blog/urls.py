66975b90fb9d2bdfd094efc1ff225ebe827c4cf4
HandBoy <handersonmedeiros@gmail.com>
2016-03-21 15:31:41 -0300

    Add views to create/edit post

--- blog/urls.py
+++ blog/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', view=views.index),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
