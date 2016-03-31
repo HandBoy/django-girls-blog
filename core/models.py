800dbe2e251e46d78a1c6d6ea66c82cec66328ed
HandBoy <handersonmedeiros@gmail.com>
2016-03-31 15:20:40 -0300

    Adicionado post detalhes e posts salvos

--- core/models.py
+++ core/models.py
@@ -20,3 +20,35 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+# Proficiency Model
+# defines one or more variables related to the knowledge,
+# skills, and abilities we wish to measure.
+class Competency(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Título')
+    root = models.BooleanField(default=False)
+    childrens = models.ManyToManyField('self')
+    created_date = models.DateTimeField(auto_now_add=True)
+
+class MatrizReferencia(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Título')
+    created_date = models.DateTimeField(auto_now_add=True)
+    competencies = models.ManyToManyField(Competency)
+
+class Task(models.Model):
+    DIFFICULTY = (
+        ('E', 'Easy'),
+        ('M', 'Medium'),
+        ('H', 'Hard'),
+    )
+    title = models.CharField(max_length=200, verbose_name='Título')
+    score = models.IntegerField;
+    difficulty = models.CharField(max_length=1, choices=DIFFICULTY)
+    user = models.ForeignKey('auth.User')
+    created_date = models.DateTimeField(auto_now_add=True)
+
+class Evidence(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Título')
+    created_date = models.DateTimeField(auto_now_add=True)
+    tasks = models.ManyToManyField(Task)
+
--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect
 from core.models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .forms import PostForm
 
 # Create your views here.
@@ -16,21 +17,21 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'post_detail.html', {'post': post})
 
-
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now()
             post.save()
             return redirect('core.views.post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'post_edit.html', {'form': form})
 
-
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -44,3 +45,20 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'post_edit.html', {'form': form})
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('core.views.post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('core.views.index')
+
+
